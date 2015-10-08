8d61d7e0f72cc1a5fe47a442a62ec42e7d41716f
zkan <kan@prontomarketing.com>
2015-10-09 06:36:05 +0700

    Prepare Vagrantfile and setup script

---
+++ setup/bootstrap.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+sudo apt-get install curl
+sudo apt-get install -y linux-image-generic-lts-trusty
+curl -sSL https://get.docker.com/gpg | sudo apt-key add -
+curl -sSL https://get.docker.com/ | sh
